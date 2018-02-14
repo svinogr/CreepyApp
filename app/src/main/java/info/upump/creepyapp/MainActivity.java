1e8836d20a33dba4748387e560c7488773e2958c
svinogr <svinogr@hotmail.com>
2018-02-14 13:51:10 +0300

    add  admob in recycler

--- app/src/main/java/info/upump/creepyapp/MainActivity.java
+++ app/src/main/java/info/upump/creepyapp/MainActivity.java
@@ -79,13 +79,16 @@ public class MainActivity extends AppCompatActivity
             CoverFragment coverFragment = CoverFragment.newInstance();
             createFragment(coverFragment);
         }
-
-        AdView mNativeAd = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
+            /*AdView mNativeAd = findViewById(R.id.adView);
+          AdRequest adRequest = new AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+             .build();
+        //You can add the following code if you are testing in an emulator
+         *//*   AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
+                .build();*//*
+          mNativeAd.loadAd(adRequest);*/
 
-        mNativeAd.loadAd(adRequest);
 
 
     }
